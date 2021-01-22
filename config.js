a01054d57bae453fc9418135d663f4ca774342c0
Michael Venturi <michaelventuri58@aol.com>
2021-01-22 10:20:25 -0500

    fixed

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://michaelventuri:8MLhSiEAuvqYEbu1@cluster0.eirmg.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
